f0052fad85e89a349e8bec70bfae754f65e48e15
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-05-07 14:20:54 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        Lightred: "hsl(0, 100%, 67%)",
+        Orangeyyellow: "hsl(39, 100%, 56%)",
+        Greenteal: "hsl(166, 100%, 37%)",
+        Cobaltblue: "hsl(234, 85%, 45%)",
+        Lightslateblue: "hsl(252, 100%, 67%)",
+        Lightroyalblue: "hsl(241, 81%, 54%)",
+        Violetblue: "hsla(256, 72%, 46%, 1)",
+        Persianblue: "hsla(241, 72%, 46%, 0)",
+        White:" hsl(0, 0%, 100%)",
+        Paleblue: "hsl(221, 100%, 96%)",
+        Lightlavender: "hsl(241, 100%, 89%)",
+        Darkgrayblue: "hsl(224, 30%, 27%)"
+      },
+      fontFamily:{
+        Hanken:['Hanken','san-serif'],
+        HankenBold:['HankenBold','san-serif'],
+        HankenMd:['HankenMd','san-serif'],
+        HankenLg:['Hanken','san-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
